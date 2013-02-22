90884c52ffe56bc9220e2baf288b7b724f06a13a
marcin- <marcin.bojara@gmail.com>
2013-02-22 16:34:40 +0100

    lvm2: add static

--- system/base/lvm2/actions.py
+++ system/base/lvm2/actions.py
@@ -14,12 +14,14 @@ from pisi.actionsapi import shelltools
 WorkDir = "LVM2.%s" % get.srcVERSION()
 
 def builddiet():
-    dietCC = "diet %s %s %s -Os -static" % (get.CC(), get.CFLAGS(), get.LDFLAGS())
+    #dietCC = "diet %s %s %s -Os -static" % (get.CC(), get.CFLAGS(), get.LDFLAGS())
+    dietCC = "%s %s %s -Os -static" % (get.CC(), get.CFLAGS(), get.LDFLAGS())
     shelltools.export("CC", dietCC)
 
     autotools.make("distclean")
     autotools.autoreconf("-fi")
-    autotools.configure("ac_cv_lib_dl_dlopen=no \
+    autotools.configure('ac_cv_lib_dl_dlopen=no \
+                         --with-staticdir="/sbin" \
                          --enable-debug \
                          --with-optimisation=\"%s -Os\" \
                          --enable-static_link \
@@ -31,19 +33,21 @@ def builddiet():
                          --enable-applib     \
                          --enable-cmdlib     \
                          --enable-pkgconfig  \
-                         --enable-udev_sync" % get.CFLAGS())
+                         --enable-udev_rules \
+                         --enable-udev_sync' % get.CFLAGS())
 
     pisitools.dosed("lib/misc/configure.h","rpl_malloc","malloc")
     pisitools.dosed("lib/misc/configure.h","rpl_realloc","realloc")
 
     autotools.make("-j1 -C include")
-    #autotools.make("-j1 -C lib LIB_SHARED= VERSIONED_SHLIB=")
-    #autotools.make("-j1 -C libdm LIB_SHARED= VERSIONED_SHLIB=")
-    #autotools.make("-j1 -C tools dmsetup.static lvm.static DIETLIBC_LIBS=\"-lcompat\"")
+    autotools.make("-j1 -C lib LIB_SHARED= VERSIONED_SHLIB=")
+    autotools.make("-j1 -C libdm LIB_SHARED= VERSIONED_SHLIB=")
+    autotools.make("-j1 -C libdaemon LIB_SHARED= VERSIONED_SHLIB=")
+    autotools.make("-j1 -C tools dmsetup.static lvm.static DIETLIBC_LIBS=\"-lcompat\"")
 
-    #pisitools.insinto("/usr/lib/dietlibc/lib-i386", "libdm/ioctl/libdevmapper.a")
-    #pisitools.insinto("/sbin/", "tools/lvm.static")
-    #pisitools.insinto("/sbin/", "tools/dmsetup.static")
+    pisitools.insinto("/usr/lib/dietlibc/lib-i386", "libdm/ioctl/libdevmapper.a")
+    pisitools.insinto("/sbin/", "tools/lvm.static")
+    pisitools.insinto("/sbin/", "tools/dmsetup.static")
 
 
 def setup():
@@ -54,6 +58,9 @@ def setup():
 
     autotools.autoreconf("-fi")
     autotools.configure("--enable-lvm1_fallback \
+                         --with-default-run-dir=/run/lvm \
+                         --with-default-locking-dir=/run/lock/lvm \
+                         --with-dmeventd-path=/sbin/dmeventd \
                          --enable-fsadm \
                          --with-pool=internal \
                          --with-user= \
@@ -64,10 +71,8 @@ def setup():
                          --with-device-uid=0 \
                          --with-device-gid=6 \
                          --with-device-mode=0660 \
-                         --enable-pkgconfig \
-                         --enable-applib \
-                         --enable-cmdlib \
                          --enable-dmeventd \
+                         --enable-udev_rules \
                          --enable-udev_sync \
                          --with-snapshots=internal \
                          --with-mirrors=internal \
@@ -77,10 +82,9 @@ def setup():
                          --disable-realtime \
                          --disable-selinux \
                          --with-confdir=/etc \
-            --enable-applib     \
-            --enable-cmdlib     \
-            --enable-pkgconfig  \
-            --enable-udev_sync" % get.sbinDIR())
+                         --enable-applib \
+                         --enable-cmdlib \
+                         --enable-pkgconfig " % get.sbinDIR())
 
     pisitools.dosed("make.tmpl","-lm","")
 
@@ -88,7 +92,7 @@ def build():
     autotools.make("-C include")
     #autotools.make("-C libdm")
     #autotools.make("-C lib")
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.rawInstall('DESTDIR=%s' % get.installDIR())
